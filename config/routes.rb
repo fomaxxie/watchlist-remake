242c8cfdc19976d19e32c6fb2c0a9712765517f3
Maksim Fomkin <fomaxxi@gmail.com>
2023-09-19 17:40:48 +0200

    watchlist remake

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
+  # # Read all
+  # get    "lists",          to: "lists#index"
+  # # Create
+  # get    "lists/new",      to: "lists#new",  as: :new_restaurant
+  # post   "lists",          to: "lists#create"
+  # # Read one - The `show` route needs to be *after* `new` route.
+  # get    "lists/:id",      to: "lists#show", as: :restaurant
+  # # Update
+  # get    "lists/:id/edit", to: "lists#edit", as: :edit_restaurant
+  # patch  "lists/:id",      to: "lists#update"
+  # # Delete
+  # delete "lists/:id",      to: "lists#destroy"
 
+  root to: "lists#index"
+  resources :lists do
+    resources :bookmarks, only: [:index, :new, :show, :create]
+  end
+end
